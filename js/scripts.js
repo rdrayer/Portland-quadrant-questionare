a1b4aa5d1bb77a0ca2b2be25f1e58ab96dc95dca
Roberta Drayer <robertadrayer@gmail.com>
2019-02-27 22:43:39 -0800

    added questions and javascript/jquery elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+  $("form#quad").submit(function() {
+    event.preventDefault();
+    var age = parseInt($("#age").val());
+    var coffee = parseInt($("input:radio[name=coffee]:checked").val());
+    var team = parseInt($("input:radio[name=team]:checked").val());
+    var brunch = parseInt($("input:radio[name=brunch]:checked").val());
+    var park = parseInt($("input:radio[name=park]:checked").val());
+    var total = age + coffee + team + brunch + park;
 
-})
+    if (total > 1 && total <= 6) {
+      $("#burbs").show();
+      $("#submit").remove();
+    } else if (total >=7 && total <=12) {
+      $("#sene").show();
+    } else if (total >=13 && total <=18) {
+      $("#nw").show();
+    } else if (total >=19 && total <=24) {
+      $("sw").show();
+    }
+  });
+});
